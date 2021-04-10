aa7cbd9ceaa2266e28445b2c7cd95f2c5e0ee011
Ahtem707 <asitzhalilov@gmail.com>
2021-04-10 10:41:45 +0300

    Рабочая демо версия

---
+++ backend/config/database.js
@@ -0,0 +1,12 @@
+import mysql from "mysql2";
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: '3308',
+    // database: 'closed library of free writers',
+    database: 'pos_db',
+    user: 'root',
+    password: 'admin'
+});
+
+export default db;
